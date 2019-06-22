3155e962391c9551a1251b2ef228719c5d3d881b
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-06-22 15:40:02 +0300

    first attempt to iterate

--- src/Turtle.elm
+++ src/Turtle.elm
@@ -1,4 +1,4 @@
-module Turtle exposing (Command(..), Point, State, applyCommand, initState)
+module Turtle exposing (Command(..), Line, Point, State, initState, iterate, toLines)
 
 import Color
 import ColorShift
@@ -17,11 +17,6 @@ type alias State =
     }
 
 
-initState : State
-initState =
-    State ( 0, 0 ) 0 1 Color.black
-
-
 type Command
     = Forward Float
     | Turn Float
@@ -30,45 +25,103 @@ type Command
     | IncreaseSaturation Float
     | IncreaseLightness Float
     | IncreaseAlpha Float
+    | List Command
 
 
-applyCommand : Command -> State -> State
-applyCommand command ({ location, orientation, width, color } as state) =
-    case command of
-        Forward x ->
-            { state
-                | location =
-                    Vector2d.fromPolarComponents ( x, orientation )
-                        |> Vector2d.sum (Vector2d.fromComponents location)
-                        |> Vector2d.components
-            }
+type alias Line =
+    { start : Point
+    , end : Point
+    , lineWidth : Float
+    , lineColor : Color.Color
+    }
 
-        Turn a ->
-            { state
-                | orientation = orientation + degrees a
-            }
 
-        IncreaseWidth x ->
-            { state
-                | width = max 0 (width + x)
-            }
+initState : State
+initState =
+    State ( 0, 0 ) 0 1 Color.black
 
-        HueShift a ->
-            { state
-                | color = ColorShift.hueShift a color
-            }
 
-        IncreaseSaturation x ->
-            { state
-                | color = ColorShift.increaseSaturation x color
-            }
+iterate : ( State, List Command ) -> ( State, List Line )
+iterate ( state, commands ) =
+    List.foldl
+        (\command ( { location, orientation, width, color } as currentState, lines ) ->
+            case command of
+                Forward x ->
+                    let
+                        newState =
+                            { currentState
+                                | location =
+                                    Vector2d.fromPolarComponents ( x, orientation )
+                                        |> Vector2d.sum (Vector2d.fromComponents location)
+                                        |> Vector2d.components
+                            }
+                    in
+                    ( newState
+                    , Line location newState.location width color :: lines
+                    )
 
-        IncreaseLightness x ->
-            { state
-                | color = ColorShift.increaseLightness x color
-            }
+                Turn a ->
+                    ( { currentState
+                        | orientation = orientation + degrees a
+                      }
+                    , lines
+                    )
 
-        IncreaseAlpha x ->
-            { state
-                | color = ColorShift.increaseAlpha x color
-            }
+                IncreaseWidth x ->
+                    ( { currentState
+                        | width = max 0 (width + x)
+                      }
+                    , lines
+                    )
+
+                HueShift a ->
+                    ( { currentState
+                        | color = ColorShift.hueShift a color
+                      }
+                    , lines
+                    )
+
+                IncreaseSaturation x ->
+                    ( { currentState
+                        | color = ColorShift.increaseSaturation x color
+                      }
+                    , lines
+                    )
+
+                IncreaseLightness x ->
+                    ( { currentState
+                        | color = ColorShift.increaseLightness x color
+                      }
+                    , lines
+                    )
+
+                IncreaseAlpha x ->
+                    ( { currentState
+                        | color = ColorShift.increaseAlpha x color
+                      }
+                    , lines
+                    )
+
+                List _ ->
+                    let
+                        ( _, branchedLines ) =
+                            iterate ( currentState, command )
+
+                        newLines =
+                            List.concat [ branchedLines, lines ]
+                    in
+                    ( currentState
+                    , newLines
+                    )
+        )
+        ( state, [] )
+        commands
+
+
+toLines : List Command -> List Line
+toLines commands =
+    let
+        ( _, lines ) =
+            iterate ( initState, commands )
+    in
+    List.reverse lines
