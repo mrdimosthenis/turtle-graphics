4d93896ee42119a750b7a5be7e02a9f37c57f4e8
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-06-30 02:49:05 +0300

    layout

--- src/TurtleGraphics.elm
+++ src/TurtleGraphics.elm
@@ -1,4 +1,4 @@
-module TurtleGraphics exposing (..)
+module TurtleGraphics exposing (Command, branch, increaseAlpha, increaseBlue, increaseGreen, increaseLightness, increaseRed, increaseSaturation, increaseWidth, move, render, rotateHue, turn)
 
 import TurtleGraphics.Core as Core
 import TurtleGraphics.Render as Render
@@ -7,17 +7,50 @@ import TurtleGraphics.Render as Render
 type alias Command =
     Core.Command
 
-move = Core.Move
-turn = Core.Turn
-increaseWidth = Core.IncreaseWidth
-rotateHue = Core.RotateHue
-increaseSaturation = Core.IncreaseSaturation
-increaseLightness = Core.IncreaseLightness
-increaseAlpha = Core.IncreaseAlpha
-increaseRed = Core.IncreaseRed
-increaseGreen = Core.IncreaseGreen
-increaseBlue = Core.IncreaseBlue
-branch = Core.Branch
+
+move =
+    Core.Move
+
+
+turn =
+    Core.Turn
+
+
+increaseWidth =
+    Core.IncreaseWidth
+
+
+rotateHue =
+    Core.RotateHue
+
+
+increaseSaturation =
+    Core.IncreaseSaturation
+
+
+increaseLightness =
+    Core.IncreaseLightness
+
+
+increaseAlpha =
+    Core.IncreaseAlpha
+
+
+increaseRed =
+    Core.IncreaseRed
+
+
+increaseGreen =
+    Core.IncreaseGreen
+
+
+increaseBlue =
+    Core.IncreaseBlue
+
+
+branch =
+    Core.Branch
+
 
 render =
     Render.render
