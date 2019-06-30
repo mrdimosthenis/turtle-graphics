2152342377c6fbc77f94f52d984224843c30629c
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-06-30 20:52:52 +0300

    type annotations on interface

--- src/TurtleGraphics.elm
+++ src/TurtleGraphics.elm
@@ -1,5 +1,7 @@
 module TurtleGraphics exposing (Command, branch, increaseAlpha, increaseBlue, increaseGreen, increaseLightness, increaseRed, increaseSaturation, increaseWidth, move, render, rotateHue, turn)
 
+import Color exposing (Color)
+import Html exposing (Html)
 import TurtleGraphics.Core as Core
 import TurtleGraphics.Render as Render
 
@@ -8,49 +10,61 @@ type alias Command =
     Core.Command
 
 
+move : Float -> Command
 move =
     Core.Move
 
 
+turn : Float -> Command
 turn =
     Core.Turn
 
 
+increaseWidth : Float -> Command
 increaseWidth =
     Core.IncreaseWidth
 
 
+rotateHue : Float -> Command
 rotateHue =
     Core.RotateHue
 
 
+increaseSaturation : Float -> Command
 increaseSaturation =
     Core.IncreaseSaturation
 
 
+increaseLightness : Float -> Command
 increaseLightness =
     Core.IncreaseLightness
 
 
+increaseAlpha : Float -> Command
 increaseAlpha =
     Core.IncreaseAlpha
 
 
+increaseRed : Float -> Command
 increaseRed =
     Core.IncreaseRed
 
 
+increaseGreen : Float -> Command
 increaseGreen =
     Core.IncreaseGreen
 
 
+increaseBlue : Float -> Command
 increaseBlue =
     Core.IncreaseBlue
 
 
+branch : List Command -> Command
 branch =
     Core.Branch
 
 
+render : Color -> Command -> Html msg
 render =
     Render.render
